7d2af5f63e5a938a11b9551cea4af7d35679b1f9
Alexey Chaichuk <alexey.chaichuk@gmail.com>
2020-11-08 21:00:48 +0500

    added fragmentActivity and 3 fragments

---
+++ app/src/main/java/ru/chaichuk/hwapp/fragments/Fragment2.kt
@@ -0,0 +1,80 @@
+package ru.chaichuk.hwapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import ru.chaichuk.hwapp.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Fragment2.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Fragment2 : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_2, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val btnFr2OpenFr1 = view!!.findViewById<Button>(R.id.btnFr2OpenFr1)
+        btnFr2OpenFr1.setOnClickListener {
+            activity!!.supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, Fragment1())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        val btnFr2OpenFr3 = view!!.findViewById<Button>(R.id.btnFr2OpenFr3)
+        btnFr2OpenFr3.setOnClickListener {
+            activity!!.supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, Fragment3())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Fragment2.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Fragment2().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
