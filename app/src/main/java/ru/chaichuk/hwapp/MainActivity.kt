d3c38756b6071819f8f97e1c5e494e4dbd18dd72
myname <me@example.com>
2020-11-19 16:28:53 +0500

    added scrollView for movie details

--- app/src/main/java/ru/chaichuk/hwapp/MainActivity.kt
+++ app/src/main/java/ru/chaichuk/hwapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package ru.chaichuk.hwapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openMovieDetails(view: View) {
+        startActivity(Intent(this, MovieDetailsActivity::class.java))
     }
 }
