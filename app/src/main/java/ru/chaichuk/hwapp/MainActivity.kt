0f85308320bdf8b244646cf4940284286571e38b
Alexey Chaichuk <alexey.chaichuk@gmail.com>
2020-11-22 19:53:45 +0500

    removed unused icons, fixed naming in layouts, removed nested layouts in movies activity

--- app/src/main/java/ru/chaichuk/hwapp/MainActivity.kt
+++ app/src/main/java/ru/chaichuk/hwapp/MainActivity.kt
@@ -4,18 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnOpenMovies = findViewById<Button>(R.id.buttonMovies);
+        btnOpenMovies.setOnClickListener {
+            startActivity(Intent(this, MoviesActivity::class.java))
+        };
+
+        val btnOpenMovieDetails = findViewById<Button>(R.id.buttonMovieDetails);
+        btnOpenMovieDetails.setOnClickListener {
+            startActivity(Intent(this, MovieDetailsActivity::class.java))
+        };
+
     }
 
-    fun openMovieDetails(view: View) {
-        startActivity(Intent(this, MovieDetailsActivity::class.java))
-    }
-
-    fun openMovies(view: View) {
-        startActivity(Intent(this, MoviesActivity::class.java))
-    }
 }
