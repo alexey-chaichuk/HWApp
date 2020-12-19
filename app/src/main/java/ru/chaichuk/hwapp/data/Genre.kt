7a2e088a37903c072ac4aa76f0892966c49bd1dd
Alexey Chaichuk <alexey.chaichuk@gmail.com>
2020-12-20 00:35:48 +0500

    moved to kotlinx.parcelize generator for data classes

--- app/src/main/java/ru/chaichuk/hwapp/data/Genre.kt
+++ app/src/main/java/ru/chaichuk/hwapp/data/Genre.kt
@@ -1,31 +1,10 @@
 package ru.chaichuk.hwapp.data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-data class Genre(val id: Int, val name: String) :Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString()!!
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(name)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Genre> {
-        override fun createFromParcel(parcel: Parcel): Genre {
-            return Genre(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Genre?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+@Parcelize
+data class Genre(
+    val id: Int,
+    val name: String
+    ) :Parcelable
--- app/src/main/java/ru/chaichuk/hwapp/data/Movie.kt
+++ app/src/main/java/ru/chaichuk/hwapp/data/Movie.kt
@@ -1,8 +1,9 @@
 package ru.chaichuk.hwapp.data
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Movie(
     val id: Int,
     val title: String,
@@ -17,49 +18,4 @@ data class Movie(
     val actors: List<Actor>,
 
     val like: Boolean
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readFloat(),
-        parcel.readInt(),
-        parcel.readInt(),
-        parcel.readInt(),
-        parcel.createTypedArrayList(Genre)!!,
-        parcel.createTypedArrayList(Actor)!!,
-        parcel.readByte() != 0.toByte()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(title)
-        parcel.writeString(overview)
-        parcel.writeString(poster)
-        parcel.writeString(backdrop)
-        parcel.writeFloat(ratings)
-        parcel.writeInt(numberOfRatings)
-        parcel.writeInt(minimumAge)
-        parcel.writeInt(runtime)
-        parcel.writeTypedList(genres)
-        parcel.writeTypedList(actors)
-        parcel.writeByte(if (like) 1 else 0)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Movie> {
-        override fun createFromParcel(parcel: Parcel): Movie {
-            return Movie(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Movie?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    ) : Parcelable
